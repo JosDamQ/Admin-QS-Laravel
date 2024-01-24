996acee2b81b4193848583054ce512426ba56f2d
jgarcia-2018163 <jgarcia-2018163@kinal.edu.gt>
2024-01-24 10:02:42 -0600

    Creacion de tablas status y packages mediante migraciones

---
+++ database/migrations/2024_01_24_152456_create_packages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('trackin')->nullable(false);
+            $table->string('weight', 30)->nullable(false);
+            $table->text('description')->nullable(false);
+            $table->unsignedBigInteger('customer_id')->nullable(false);
+            $table->unsignedBigInteger('status_id')->nullable(false);
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('status_id')->references('id')->on('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('packages');
+    }
+};
