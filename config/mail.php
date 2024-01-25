90dcbe478e78bd8a193092f2ae4cdddbb0f4caec
jgarcia-2018163 <jgarcia-2018163@kinal.edu.gt>
2024-01-24 19:19:58 -0600

    Se implemento breezer y se configuro la confirmacion del correo para los usuarios

--- config/mail.php
+++ config/mail.php
@@ -37,14 +37,30 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            /*'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'local_domain' => env('MAIL_EHLO_DOMAIN'),*/
+            'host' => env('MAIL_HOST', 'localhost'),
+            'port' => env('MAIL_PORT', 25),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+
+            'verify_peer' => false,
         ],
+
+        /*'stream' => [
+            'ssl' => [
+                'allow_self_signed' => true,
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ],
+        ],*/
 
         'ses' => [
             'transport' => 'ses',
